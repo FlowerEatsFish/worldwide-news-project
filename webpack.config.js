d279322bd7b3551e76ae853566a06075a7cf0d75
Eating-Fish Flower <flower.eating.fish@gmail.com>
2018-12-03 17:25:01 +0800

    Initial workshop

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  mode: "development",
+  entry: "./src/index.tsx",
+  output: {
+    filename: "bundle.js",
+    path: __dirname + "/dist"
+  },
+  devtool: "source-map",
+  resolve: {
+    extensions: [".ts", ".tsx", ".js", ".json"]
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: "ts-loader"
+      },
+      {
+        enforce: "pre",
+        test: /\.js$/,
+        loader: "source-map-loader"
+      }
+    ]
+  }
+};
